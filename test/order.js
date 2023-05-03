c7478c77b9f77d54529cdc3955ca4398cddca277
Denis Poplavskii <normal64@mail.ru>
2023-05-03 22:11:18 +0200

    feat: update for new version; new test

--- test/order.js
+++ test/order.js
@@ -1,10 +1,10 @@
 const versionMatcher = require("chromedriver-version-matcher");
-
+const { expect } = require("chai");
 const { By, Key, Builder, Select, until } = require("selenium-webdriver");
 
 require("chromedriver");
 describe("task for go phptravels", async function () {
-  it("automate all links", async function () {
+  it("successful", async function () {
     //create driver
     // Set up the Selenium driver
     const driver = await new Builder().forBrowser("chrome").build();
@@ -35,6 +35,13 @@ describe("task for go phptravels", async function () {
 
     await driver.findElement(By.id("submit-button")).click();
 
+      //update for new functionality on page
+    await driver.wait(
+      until.urlContains("https://www.graze.com/uk/registration/address"),
+      5000
+    );
+    await driver.findElement(By.className("alert-link")).click();
+
     // Wait for the new page to load
     await driver.wait(
       until.urlContains("https://www.graze.com/uk/registration/address"),
@@ -49,11 +56,12 @@ describe("task for go phptravels", async function () {
     await postcode.sendKeys("TW9 1EW");
     await driver.findElement(By.id("continue")).click();
 
-    //filling second data page
+    //filling third data page
     await driver.wait(
       until.urlContains("https://www.graze.com/uk/registration/card-details"),
       5000
     );
+
     const cardHolder = await driver.findElement(By.id("nameOnCard"));
     await cardHolder.sendKeys("Newyork Englishman");
 
@@ -62,7 +70,7 @@ describe("task for go phptravels", async function () {
     await cardNumber.sendKeys("6270");
     await cardNumber.sendKeys("4100");
     await cardNumber.sendKeys("7923");
-    
+
     const cvc = await driver.findElement(By.id("cv2"));
     await cvc.sendKeys("127");
     const billingCode = await driver.findElement(By.id("billingPostcode"));
@@ -71,5 +79,72 @@ describe("task for go phptravels", async function () {
     setTimeout(async function () {
       await driver.quit();
     }, 5000);
+  });
+
+  it("successful", async function () {
+    //create driver
+    // Set up the Selenium driver
+    const driver = await new Builder().forBrowser("chrome").build();
+
+    // Navigate to the webpage
+    driver.get("https://www.graze.com/uk/");
+
+    await driver.manage().window().maximize();
+    await driver
+      .findElement(
+        By.className("button button--primary signup-button text-center")
+      )
+      .click();
+    const boxes = await driver.findElements(
+      By.className("button button--secondary button--block")
+    );
+
+    await boxes[1].click();
+
+    //filling first data page with info
+    const firstName = await driver.findElement(By.id("firstName"));
+
+    const secondName = await driver.findElement(By.id("lastName"));
+
+    const email = await driver.findElement(By.id("email"));
+
+    const password = await driver.findElement(By.id("password"));
+
+    await driver.findElement(By.id("submit-button")).click();
+
+    driver.wait(until.stalenessOf(boxes[1]));
+    // Find all  elements with the "help-block" class
+    const helpBlockElements = await driver.findElements(By.css("small"));
+
+    // Get the second <small> element and output its text content
+    const firstHelpBlockElement = helpBlockElements[0];
+    //getting error text for first name
+    const firstHelpBlockText = await firstHelpBlockElement.getAttribute(
+      "innerText"
+    );
+    console.log(`firstHelpBlockText`, firstHelpBlockText);
+
+    const secondHelpBlockElement = helpBlockElements[1];
+    //getting error text for first name
+    const secondHelpBlockText = await secondHelpBlockElement.getAttribute(
+      "innerText"
+    );
+    console.log(`secondHelpBlockText`, secondHelpBlockText);
+
+    // // Check if the text matches the expected value
+    //first name field error
+    const firstNameHelpBlockExpected = "first name is required";
+    expect(firstHelpBlockText).to.equal(firstNameHelpBlockExpected);
+    //second name field error
+    const secondNameHelpBlockExpected = "last name is required";
+    expect(secondHelpBlockText).to.equal(secondNameHelpBlockExpected);
+
+    // await firstName.sendKeys("Denis");
+    // await secondName.sendKeys("Poplavskii");
+    // await email.sendKeys("mymail@gmail.com");
+    // await password.sendKeys("easy1357$");
+    // setTimeout(async function () {
+    //   await driver.quit();
+    // }, 5000);
   });
 });
