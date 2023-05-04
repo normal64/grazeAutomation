45554f04d2f32fb3a0ed27a3007209fbf8d6f0a3
Denis Poplavskii <normal64@mail.ru>
2023-05-04 18:55:36 +0200

    feat: empty fields check

--- test/order.js
+++ test/order.js
@@ -2,8 +2,86 @@ const versionMatcher = require("chromedriver-version-matcher");
 const { expect } = require("chai");
 const { By, Key, Builder, Select, until } = require("selenium-webdriver");
 
+
 require("chromedriver");
 describe("task for go phptravels", async function () {
+  // it("successful", async function () {
+  //   //create driver
+  //   // Set up the Selenium driver
+  //   const driver = await new Builder().forBrowser("chrome").build();
+
+  //   // Navigate to the webpage
+  //   driver.get("https://www.graze.com/uk/");
+
+  //   await driver.manage().window().maximize();
+  //   await driver
+  //     .findElement(
+  //       By.className("button button--primary signup-button text-center")
+  //     )
+  //     .click();
+  //   const boxes = await driver.findElements(
+  //     By.className("button button--secondary button--block")
+  //   );
+
+  //   await boxes[1].click();
+  //   //filling first data page with info
+  //   const firstName = await driver.findElement(By.id("firstName"));
+  //   await firstName.sendKeys("Denis");
+  //   const secondName = await driver.findElement(By.id("lastName"));
+  //   await secondName.sendKeys("Poplavskii");
+  //   const email = await driver.findElement(By.id("email"));
+  //   await email.sendKeys("mymail@gmail.com");
+  //   const password = await driver.findElement(By.id("password"));
+  //   await password.sendKeys("easy1357$");
+
+  //   await driver.findElement(By.id("submit-button")).click();
+
+  //     //update for new functionality on page
+  //   await driver.wait(
+  //     until.urlContains("https://www.graze.com/uk/registration/address"),
+  //     5000
+  //   );
+  //   await driver.findElement(By.className("alert-link")).click();
+
+  //   // Wait for the new page to load
+  //   await driver.wait(
+  //     until.urlContains("https://www.graze.com/uk/registration/address"),
+  //     5000
+  //   );
+  //   //filling second data page
+  //   const streetAdress = await driver.findElement(By.id("line1"));
+  //   await streetAdress.sendKeys("Rakowicka 12");
+  //   const townName = await driver.findElement(By.id("town"));
+  //   await townName.sendKeys("Cracow");
+  //   const postcode = await driver.findElement(By.id("postcode"));
+  //   await postcode.sendKeys("TW9 1EW");
+  //   await driver.findElement(By.id("continue")).click();
+
+  //   //filling third data page
+  //   await driver.wait(
+  //     until.urlContains("https://www.graze.com/uk/registration/card-details"),
+  //     5000
+  //   );
+
+  //   const cardHolder = await driver.findElement(By.id("nameOnCard"));
+  //   await cardHolder.sendKeys("Newyork Englishman");
+
+  //   const cardNumber = await driver.findElement(By.id("number"));
+  //   await cardNumber.sendKeys("4739");
+  //   await cardNumber.sendKeys("6270");
+  //   await cardNumber.sendKeys("4100");
+  //   await cardNumber.sendKeys("7923");
+
+  //   const cvc = await driver.findElement(By.id("cv2"));
+  //   await cvc.sendKeys("127");
+  //   const billingCode = await driver.findElement(By.id("billingPostcode"));
+  //   await billingCode.sendKeys("TW9 1EW");
+  //   await driver.findElement(By.id("submit-button")).click();
+  //   setTimeout(async function () {
+  //     await driver.quit();
+  //   }, 5000);
+  // });
+
   it("successful", async function () {
     //create driver
     // Set up the Selenium driver
@@ -23,83 +101,6 @@ describe("task for go phptravels", async function () {
     );
 
     await boxes[1].click();
-    //filling first data page with info
-    const firstName = await driver.findElement(By.id("firstName"));
-    await firstName.sendKeys("Denis");
-    const secondName = await driver.findElement(By.id("lastName"));
-    await secondName.sendKeys("Poplavskii");
-    const email = await driver.findElement(By.id("email"));
-    await email.sendKeys("mymail@gmail.com");
-    const password = await driver.findElement(By.id("password"));
-    await password.sendKeys("easy1357$");
-
-    await driver.findElement(By.id("submit-button")).click();
-
-      //update for new functionality on page
-    await driver.wait(
-      until.urlContains("https://www.graze.com/uk/registration/address"),
-      5000
-    );
-    await driver.findElement(By.className("alert-link")).click();
-
-    // Wait for the new page to load
-    await driver.wait(
-      until.urlContains("https://www.graze.com/uk/registration/address"),
-      5000
-    );
-    //filling second data page
-    const streetAdress = await driver.findElement(By.id("line1"));
-    await streetAdress.sendKeys("Rakowicka 12");
-    const townName = await driver.findElement(By.id("town"));
-    await townName.sendKeys("Cracow");
-    const postcode = await driver.findElement(By.id("postcode"));
-    await postcode.sendKeys("TW9 1EW");
-    await driver.findElement(By.id("continue")).click();
-
-    //filling third data page
-    await driver.wait(
-      until.urlContains("https://www.graze.com/uk/registration/card-details"),
-      5000
-    );
-
-    const cardHolder = await driver.findElement(By.id("nameOnCard"));
-    await cardHolder.sendKeys("Newyork Englishman");
-
-    const cardNumber = await driver.findElement(By.id("number"));
-    await cardNumber.sendKeys("4739");
-    await cardNumber.sendKeys("6270");
-    await cardNumber.sendKeys("4100");
-    await cardNumber.sendKeys("7923");
-
-    const cvc = await driver.findElement(By.id("cv2"));
-    await cvc.sendKeys("127");
-    const billingCode = await driver.findElement(By.id("billingPostcode"));
-    await billingCode.sendKeys("TW9 1EW");
-    await driver.findElement(By.id("submit-button")).click();
-    setTimeout(async function () {
-      await driver.quit();
-    }, 5000);
-  });
-
-  it("successful", async function () {
-    //create driver
-    // Set up the Selenium driver
-    const driver = await new Builder().forBrowser("chrome").build();
-
-    // Navigate to the webpage
-    driver.get("https://www.graze.com/uk/");
-
-    await driver.manage().window().maximize();
-    await driver
-      .findElement(
-        By.className("button button--primary signup-button text-center")
-      )
-      .click();
-    const boxes = await driver.findElements(
-      By.className("button button--secondary button--block")
-    );
-
-    await boxes[1].click();
 
     //filling first data page with info
     const firstName = await driver.findElement(By.id("firstName"));
@@ -111,8 +112,8 @@ describe("task for go phptravels", async function () {
     const password = await driver.findElement(By.id("password"));
 
     await driver.findElement(By.id("submit-button")).click();
-
-    driver.wait(until.stalenessOf(boxes[1]));
+    //waiting for page to reload because button triggers page refresh and error elements are not present before that
+    await driver.wait(until.stalenessOf(driver.findElement(By.tagName('html'))));
     // Find all  elements with the "help-block" class
     const helpBlockElements = await driver.findElements(By.css("small"));
 
@@ -124,12 +125,26 @@ describe("task for go phptravels", async function () {
     );
     console.log(`firstHelpBlockText`, firstHelpBlockText);
 
-    const secondHelpBlockElement = helpBlockElements[1];
-    //getting error text for first name
+    const secondHelpBlockElement = helpBlockElements[2];
+    //getting error text for second name
     const secondHelpBlockText = await secondHelpBlockElement.getAttribute(
       "innerText"
     );
     console.log(`secondHelpBlockText`, secondHelpBlockText);
+
+    const emailHelpBlockElement = helpBlockElements[5];
+    //getting error text for email
+    const emailHelpBlockText = await emailHelpBlockElement.getAttribute(
+      "innerText"
+    );
+    console.log(`emailHelpBlockText1`, emailHelpBlockText);
+
+    const passwordHelpBlockElement = helpBlockElements[8];
+    //getting error text for password
+    const passwordHelpBlockText = await passwordHelpBlockElement.getAttribute(
+      "innerText"
+    );
+    console.log(`passwordHelpBlockText1`, passwordHelpBlockText);
 
     // // Check if the text matches the expected value
     //first name field error
@@ -138,6 +153,12 @@ describe("task for go phptravels", async function () {
     //second name field error
     const secondNameHelpBlockExpected = "last name is required";
     expect(secondHelpBlockText).to.equal(secondNameHelpBlockExpected);
+
+    const emailHelpBlockExpected = "This field is required";
+    expect(emailHelpBlockText).to.equal(emailHelpBlockExpected);
+
+    const passwordHelpBlockExpected = "This field is required";
+    expect(passwordHelpBlockText).to.equal(passwordHelpBlockExpected);
 
     // await firstName.sendKeys("Denis");
     // await secondName.sendKeys("Poplavskii");
