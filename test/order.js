89a045057a119f272a736f0dea497cf6a5b9d46c
Denis Poplavskii <normal64@mail.ru>
2023-05-07 18:57:40 +0200

    unc

--- test/order.js
+++ test/order.js
@@ -4,82 +4,82 @@ const { By, Key, Builder, Select, until } = require("selenium-webdriver");
 
 require("chromedriver");
 describe("task for go phptravels", async function () {
-  // it("successful", async function () {
-  //   //create driver
-  //   // Set up the Selenium driver
-  //   const driver = await new Builder().forBrowser("chrome").build();
+  it("successful", async function () {
+    //create driver
+    // Set up the Selenium driver
+    const driver = await new Builder().forBrowser("chrome").build();
 
-  //   // Navigate to the webpage
-  //   driver.get("https://www.graze.com/uk/");
+    // Navigate to the webpage
+    driver.get("https://www.graze.com/uk/");
 
-  //   await driver.manage().window().maximize();
-  //   await driver
-  //     .findElement(
-  //       By.className("button button--primary signup-button text-center")
-  //     )
-  //     .click();
-  //   const boxes = await driver.findElements(
-  //     By.className("button button--secondary button--block")
-  //   );
+    await driver.manage().window().maximize();
+    await driver
+      .findElement(
+        By.className("button button--primary signup-button text-center")
+      )
+      .click();
+    const boxes = await driver.findElements(
+      By.className("button button--secondary button--block")
+    );
 
-  //   await boxes[1].click();
-  //   //filling first data page with info
-  //   const firstName = await driver.findElement(By.id("firstName"));
-  //   await firstName.sendKeys("Denis");
-  //   const secondName = await driver.findElement(By.id("lastName"));
-  //   await secondName.sendKeys("Poplavskii");
-  //   const email = await driver.findElement(By.id("email"));
-  //   await email.sendKeys("mymail@gmail.com");
-  //   const password = await driver.findElement(By.id("password"));
-  //   await password.sendKeys("easy1357$");
+    await boxes[1].click();
+    //filling first data page with info
+    const firstName = await driver.findElement(By.id("firstName"));
+    await firstName.sendKeys("Denis");
+    const secondName = await driver.findElement(By.id("lastName"));
+    await secondName.sendKeys("Poplavskii");
+    const email = await driver.findElement(By.id("email"));
+    await email.sendKeys("mymail@gmail.com");
+    const password = await driver.findElement(By.id("password"));
+    await password.sendKeys("easy1357$");
 
-  //   await driver.findElement(By.id("submit-button")).click();
+    await driver.findElement(By.id("submit-button")).click();
 
-  //   //update for new functionality on page
-  //   await driver.wait(
-  //     until.urlContains("https://www.graze.com/uk/registration/address"),
-  //     5000
-  //   );
-  //   await driver.findElement(By.className("alert-link")).click();
+    //update for new functionality on page
+    await driver.wait(
+      until.urlContains("https://www.graze.com/uk/registration/address"),
+      5000
+    );
+    await driver.findElement(By.className("alert-link")).click();
 
-  //   // Wait for the new page to load
-  //   await driver.wait(
-  //     until.urlContains("https://www.graze.com/uk/registration/address"),
-  //     5000
-  //   );
-  //   //filling second data page
-  //   const streetAdress = await driver.findElement(By.id("line1"));
-  //   await streetAdress.sendKeys("Rakowicka 12");
-  //   const townName = await driver.findElement(By.id("town"));
-  //   await townName.sendKeys("Cracow");
-  //   const postcode = await driver.findElement(By.id("postcode"));
-  //   await postcode.sendKeys("TW9 1EW");
-  //   await driver.findElement(By.id("continue")).click();
+    // Wait for the new page to load
+    await driver.wait(
+      until.urlContains("https://www.graze.com/uk/registration/address"),
+      5000
+    );
+    //filling second data page
+    const streetAdress = await driver.findElement(By.id("line1"));
+    await streetAdress.sendKeys("Rakowicka 12");
+    const townName = await driver.findElement(By.id("town"));
+    await townName.sendKeys("Cracow");
+    const postcode = await driver.findElement(By.id("postcode"));
+    await postcode.sendKeys("TW9 1EW");
+    await driver.findElement(By.id("continue")).click();
 
-  //   //filling third data page
-  //   await driver.wait(
-  //     until.urlContains("https://www.graze.com/uk/registration/card-details"),
-  //     5000
-  //   );
+    //filling third data page
+    await driver.wait(
+      until.urlContains("https://www.graze.com/uk/registration/card-details"),
+      5000
+    );
 
-  //   const cardHolder = await driver.findElement(By.id("nameOnCard"));
-  //   await cardHolder.sendKeys("Newyork Englishman");
+    const cardHolder = await driver.findElement(By.id("nameOnCard"));
+    await cardHolder.sendKeys("Newyork Englishman");
 
-  //   const cardNumber = await driver.findElement(By.id("number"));
-  //   await cardNumber.sendKeys("4739");
-  //   await cardNumber.sendKeys("6270");
-  //   await cardNumber.sendKeys("4100");
-  //   await cardNumber.sendKeys("7923");
+    const cardNumber = await driver.findElement(By.id("number"));
+    await cardNumber.sendKeys("4739");
+    await cardNumber.sendKeys("6270");
+    await cardNumber.sendKeys("4100");
+    await cardNumber.sendKeys("7923");
 
-  //   const cvc = await driver.findElement(By.id("cv2"));
-  //   await cvc.sendKeys("127");
-  //   const billingCode = await driver.findElement(By.id("billingPostcode"));
-  //   await billingCode.sendKeys("TW9 1EW");
-  //   await driver.findElement(By.id("submit-button")).click();
-  //   setTimeout(async function () {
-  //     await driver.quit();
-  //   }, 5000);
-  // });
+    const cvc = await driver.findElement(By.id("cv2"));
+    await cvc.sendKeys("127");
+    const billingCode = await driver.findElement(By.id("billingPostcode"));
+    await billingCode.sendKeys("TW9 1EW");
+    await driver.findElement(By.id("submit-button")).click();
+    setTimeout(async function () {
+      await driver.quit();
+    }, 5000);
+  });
 
   it("falsy form fields", async function () {
     //create driver
